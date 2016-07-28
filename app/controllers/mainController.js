0e45ce415764552ef75baaaa779f7212d63f289f
SwanBit Team <swanbitteam@gmail.com>
2016-07-28 14:52:52 +0530

    Showing Employees and adding filter

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,6 +1,37 @@
+app.factory('EmployeesService',function($http){
+    return {
+        getEmployees:function(){
+            var url = "http://app.konacloud.io/api/team/testgular/Employee";
+            var req = ({
+                url :url,
+                method:'GET',
+            });
+            return $http(req);
+        }
+    };
+})
 
 
+app.controller('MainController', function ($scope, EmployeesService) {
+    // Getting all the Employees List
+    $scope.employees = {};
+    $scope.loadingEmployees = false;
+    
+    function getEmployees(){
+        $scope.loadingEmployees = true;
+        console.log("Fecthing Employees");
+        EmployeesService.getEmployees().success(function(data, status, header, config) {
+            console.log(data);
+            $scope.loadingEmployees = false;
+            $scope.employees = data;
+        }).error(function(data, status, header, config) {
+            $scope.loadingEmployees = false;
+            alert("There was an error fetching the list!");
+        });
+    }
+    getEmployees();
+    
+    
 
-app.controller('MainController', function ($scope) {
 
 });
